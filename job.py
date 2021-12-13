3ee22299093240d350ef840626c1eab582bf030c
Thorhian <thorhian@gmail.com>
2021-12-12 22:38:35 -0700

    Encapsulated rendering code to Job class.
    TODO: Make it render it in multiple slices (per depth of cut),
    which will require some sort of loop and the ability to change
    the orthographic matrix.
    
    TODO: The ability to take a function and use that function
    to generate a tool path from the generated images.

---
+++ job.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+import numpy as np
+from stl import mesh
+import moderngl
+import glm
+from PIL import Image
+
+import Helper_Functions as hf
+
+
+class Job:
+    '''
+    A class to handle the setup of the rendering of a targetted object
+    and generating tool paths when a generator is provided.
+    '''
+
+    def __init__(self, target_model, obstacles: list, tool_diam: float,
+                 target_res: float = 0.1):
+        self.target_model = target_model
+        self.obstacles = obstacles
+        self.tool_diam = tool_diam
+        self.target_res = target_res
+        self.bounds = self.calculate_bounds()
+        self.img_res = self.calculate_resolution(self.bounds)
+        print(self.img_res)
+        self.ctx = moderngl.create_standalone_context()
+        self.setup_opengl_objects()
+
+
+    def calculate_bounds(self):
+        '''
+        Calculates the bounding box of the targetted model. Returns a
+        tuple of (-x, x, -y, y, -z, z).
+        '''
+        rough_bounds = hf.get_model_min_max(self.target_model)
+        lower_bounds_with_margin = np.floor(rough_bounds[0::2]) - math.ceil(self.tool_diam + 5)
+        higher_bounds_with_margin = np.ceil(rough_bounds[1::2]) + math.ceil(self.tool_diam + 5)
+        return (lower_bounds_with_margin[0], higher_bounds_with_margin[0],
+                lower_bounds_with_margin[1], higher_bounds_with_margin[1],
+                lower_bounds_with_margin[2], higher_bounds_with_margin[2])
+
+    def calculate_resolution(self, bounds):
+        '''
+        Determines resolution of the images to be rendered.
+        '''
+        return (math.ceil((bounds[1] - bounds[0]) / self.target_res),
+                math.ceil((bounds[3] - bounds[2]) / self.target_res))
+
+    def setup_opengl_objects(self):
+        '''
+        Creates VBOs, Programs, VAOs, and FBOs for the job.
+        '''
+
+        self.ctx.enable(moderngl.DEPTH_TEST)
+        model_vertex_shader = hf.load_shader("./v_shader.vert")
+        model_frag_shader = hf.load_shader("./frag_shader.frag")
+
+        model_render_prog = self.ctx.program(vertex_shader=model_vertex_shader,
+                                             fragment_shader=model_frag_shader)
+
+        image_vertex_shader = hf.load_shader("./image_shader.vert")
+        edge_frag_shader = hf.load_shader("./image_shader.frag")
+
+        edge_detection_prog = self.ctx.program(vertex_shader=image_vertex_shader,
+                                               fragment_shader=edge_frag_shader)
+
+        edge_expand_frag_shader = hf.load_shader("./edge_expand.frag")
+
+        edge_expand_prog = self.ctx.program(vertex_shader=image_vertex_shader,
+                                            fragment_shader=edge_expand_frag_shader)
+
+        #Create Textures
+        firstPass = self.ctx.texture(self.img_res, 4)
+        firstPassDepth = self.ctx.depth_texture(self.img_res)
+        secondPass = self.ctx.texture(self.img_res, 4)
+        secondPassDepth = self.ctx.depth_texture(self.img_res)
+
+        #Projection and View Matrices
+        model_render_prog["projectionMatrix"].write(
+            glm.ortho(self.bounds[0], self.bounds[1], self.bounds[2],
+                      self.bounds[3], self.bounds[4], self.bounds[5])
+        )
+        model_render_prog["viewMatrix"].write(glm.rotate(hf.deg_to_rad(0), glm.vec3(1.0, 0.0, 0.0)))
+
+        #Get textures properly assigned to uniform samplers
+        edge_detection_prog["prev_render"] = 4
+        firstPass.use(location=4)
+        edge_expand_prog["prev_render"] = 3
+        secondPass.use(location=3)
+
+        #Calculate cutter radius in pixels for edge expand algorithm
+        edge_expand_prog["cutterRadius"] = (self.tool_diam / 2) / self.target_res
+
+        #Get vertice and color data prepared
+        model_size = self.target_model.vectors.size
+        r = np.zeros(model_size)
+        g = np.zeros(model_size)
+        b = np.ones(model_size)
+        model_colors = np.dstack([r, g, b])
+        image_vertices = np.array([
+            -1, 1,
+            -1, -1,
+            1, 1,
+            1, -1,
+        ], dtype='f4')
+        model_verts = self.target_model.vectors.flatten().astype('f4').tobytes()
+        vbo = self.ctx.buffer(model_verts)
+        color_buffer = self.ctx.buffer(model_colors.astype('f4').tobytes())
+        image_vbo = self.ctx.buffer(image_vertices)
+
+
+        #Create Vertex Array Objects
+        self.vao1 = self.ctx.vertex_array(model_render_prog, [
+            (vbo, '3f', 'in_vert'),
+            (color_buffer, '3f', 'in_color'),
+        ])
+
+        self.vao2 = self.ctx.vertex_array(edge_detection_prog, [
+            (image_vbo, '2f', 'in_position'),
+        ])
+
+        self.vao3 = self.ctx.vertex_array(edge_expand_prog, [
+            (image_vbo, '2f', 'in_position'),
+        ])
+
+        self.fbo1 = self.ctx.framebuffer([firstPass], firstPassDepth)
+        self.fbo2 = self.ctx.framebuffer([secondPass], secondPassDepth)
+        self.fbo3 = self.ctx.simple_framebuffer(self.img_res)
+        self.fbo1.clear(0.0, 0.0, 0.0, 1.0)
+        self.fbo2.clear(0.0, 0.0, 0.0, 1.0)
+        self.fbo3.clear(0.0, 0.0, 0.0, 1.0)
+
+    def render(self):
+        self.fbo1.clear(0.0, 0.0, 0.0, 1.0)
+        self.fbo1.use()
+        self.vao1.render(moderngl.TRIANGLES)
+        self.fbo2.clear(0.0, 0.0, 0.0, 1.0)
+        self.fbo2.use()
+        self.vao2.render(moderngl.TRIANGLE_STRIP)
+        self.fbo3.clear(0.0, 0.0, 0.0, 1.0)
+        self.fbo3.use()
+        self.vao3.render(moderngl.TRIANGLE_STRIP)
--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-import math
-import numpy as np
 from stl import mesh
-import moderngl
-import glm
 from PIL import Image
 
-import Helper_Functions as hf
+from job import Job
 
 # Units should be in Metric.
 tool_diameter = float(8.0)
 target_res_per_pixel = 0.1 #Width/Height of each pixel
-
-tool_radius_adjusted = (tool_diameter / 2) / target_res_per_pixel
-
-print(hf.bounding_box_circle((-200.0, 400.0), 8.0))
 
 if len(sys.argv) <= 1:
     print("Please specify an STL file.\n")
@@ -25,128 +17,8 @@ if len(sys.argv) <= 1:
 #Load STL File
 stlFileName = sys.argv[1]
 model_mesh = mesh.Mesh.from_file(stlFileName)
-model_size = model_mesh.vectors.size
 
-
-#Calculate Bounding Box dimensions and Image Resolution
-bounds = hf.get_model_min_max(model_mesh)
-lower_bounds_with_margin = np.floor(bounds[0::2]) - math.ceil(tool_diameter + 5)
-higher_bounds_with_margin = np.ceil(bounds[1::2]) + math.ceil(tool_diameter + 5)
-image_res = (math.ceil((higher_bounds_with_margin[0] - lower_bounds_with_margin[0]) / target_res_per_pixel),
-        math.ceil((higher_bounds_with_margin[1] - lower_bounds_with_margin[1]) / target_res_per_pixel))
-
-#Calculates fairly exact pixel dimensions in mm, width and height
-pixel_dimensions = (((higher_bounds_with_margin[0] - lower_bounds_with_margin[0]) / image_res[0]),
-        ((higher_bounds_with_margin[1] - lower_bounds_with_margin[1]) / image_res[1]))
-
-print((higher_bounds_with_margin[0] - lower_bounds_with_margin[0]),
-        (higher_bounds_with_margin[1] - lower_bounds_with_margin[1]))
-print(image_res)
-print(pixel_dimensions)
-
-#Create OpenGL context
-ctx = moderngl.create_standalone_context()
-
-#Load shader code files
-vert_shader = hf.load_shader("./v_shader.vert")
-frag_shader = hf.load_shader("./frag_shader.frag")
-
-vert2_shader = hf.load_shader("./image_shader.vert")
-frag2_shader = hf.load_shader("./image_shader.frag")
-
-frag3_shader = hf.load_shader("./edge_expand.frag")
-
-#Create OpenGL programs with loaded shaders, compile
-prog1 = ctx.program(vertex_shader=vert_shader,
-        fragment_shader=frag_shader)
-
-prog2 = ctx.program(vertex_shader=vert2_shader,
-        fragment_shader=frag2_shader)
-
-prog3 = ctx.program(vertex_shader=vert2_shader,
-                    fragment_shader=frag3_shader)
-
-#Set model color
-r = np.zeros(model_size)
-g = np.zeros(model_size)
-b = np.ones(model_size)
-color_values = np.dstack([r, g, b])
-
-#Flatten array of vertices from the stl and prepare for opengl
-model_vertices = model_mesh.vectors.flatten().astype('f4').tobytes()
-
-#Image Mesh for edge detection phase
-image_vertices = np.array([
-    -1, 1,
-    -1, -1,
-    1, 1,
-    1, -1,
-    ], dtype='f4')
-
-
-#Create Texture(s)
-firstPass = ctx.texture(image_res, 4)
-firstPassDepth = ctx.depth_texture(image_res)
-
-secondPass = ctx.texture(image_res, 4)
-secondPassDepth = ctx.depth_texture(image_res)
-
-ctx.enable(moderngl.DEPTH_TEST)
-
-#Create Orthographic Projection Matrix and View Matrix
-prog1["projectionMatrix"].write(glm.ortho(lower_bounds_with_margin[0],
-    higher_bounds_with_margin[0],
-    lower_bounds_with_margin[1],
-    higher_bounds_with_margin[1],
-    lower_bounds_with_margin[2],
-    higher_bounds_with_margin[2]))
-prog1["viewMatrix"].write(glm.rotate(hf.deg_to_rad(0), glm.vec3(1.0, 0.0, 0.0)))
-
-prog2["prev_render"] = 4
-firstPass.use(location=4)
-
-prog3["prev_render"] = 3
-secondPass.use(location=3)
-
-prog3["cutterRadius"] = tool_radius_adjusted
-
-#Create buffers for model vertices and color values
-vbo = ctx.buffer(model_vertices)
-color_buffer = ctx.buffer(color_values.astype('f4').tobytes())
-image_vbo = ctx.buffer(image_vertices)
-
-#Create Vertex Array Object
-vao = ctx.vertex_array(prog1, [
-    (vbo, '3f', 'in_vert'),
-    (color_buffer, '3f', 'in_color'),
-    ])
-
-vao2 = ctx.vertex_array(prog2, [
-    (image_vbo, '2f', 'in_position'),
-    ])
-
-vao3 = ctx.vertex_array(prog3, [
-    (image_vbo, '2f', 'in_position'),
-    ])
-
-#Create, use, and render to Framebuffer Object (FBO)
-fbo = ctx.framebuffer([firstPass], firstPassDepth)
-fbo.use()
-fbo.clear(0.0, 0.0, 0.0, 1.0)
-vao.render(moderngl.TRIANGLES)
-
-fbo2 = ctx.framebuffer([secondPass], secondPassDepth)
-fbo2.use()
-fbo2.clear(0.0, 0.0, 0.0, 1.0)
-vao2.render(moderngl.TRIANGLE_STRIP)
-
-fbo3 = ctx.simple_framebuffer(image_res)
-fbo3.use()
-fbo3.clear(0.0, 0.0, 0.0, 1.0)
-vao3.render(moderngl.TRIANGLE_STRIP)
-
-#Render image from FBO
-final_render = Image.frombytes('RGB', fbo2.size, fbo3.read(), 'raw', 'RGB', 0, -1)
-
-final_render.save("./temp.png")
-#final_render.show()
+newJob = Job(model_mesh, [], tool_diameter, target_res=target_res_per_pixel)
+newJob.render()
+job_render = Image.frombytes('RGB', newJob.fbo3.size, newJob.fbo3.read(), 'raw', 'RGB', 0, -1)
+job_render.save("./temp.png")
