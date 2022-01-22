a87e6af406d7c8a2be4f737ce7db1bed9c23d7c7
Thorhian <thorhian@gmail.com>
2022-01-22 13:35:05 -0700

    Added box generator and moved geo gens to sep file

--- Helper_Functions.py
+++ Helper_Functions.py
@@ -7,17 +7,6 @@ import math
 def print_help():
     print("File, Depth of Cut, Tool Diameter (all units in mm).")
 
-'''
-Generates a series of lines forming a helix.
-'''
-def generate_helix(iterations, depth, diameter):
-    z = np.linspace(0, depth, iterations)
-    x = diameter * np.cos(z)
-    y = diameter * np.sin(z)
-    w = np.ones(iterations)
-
-    return np.dstack([x, y, z, w])
-
 def deg_to_rad(degrees):
     return (degrees * np.pi) / 180.0
 
---
+++ geometry_gens.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def generate_helix(iterations, depth, diameter):
+    '''
+    Generates a series of lines forming a helix.
+    '''
+    z = np.linspace(0, depth, iterations)
+    x = diameter * np.cos(z)
+    y = diameter * np.sin(z)
+    w = np.ones(iterations)
+
+    return np.dstack([x, y, z, w])
+
+def generate_box(dims: tuple):
+    '''
+    Generates a box to use for stock
+    Expects tuple to be in this format: (-x, x, -y, y, -z, z)
+    '''
+    box = np.array([
+        #Left face
+        [
+            [dims[0], dims[2], dims[4]],
+            [dims[0], dims[3], dims[4]],
+            [dims[0], dims[3], dims[5]]
+        ],
+        [
+            [dims[0], dims[2], dims[4]],
+            [dims[0], dims[2], dims[5]],
+            [dims[0], dims[3], dims[5]]
+        ],
+        #Z bottom face
+        [
+            [dims[0], dims[2], dims[4]],
+            [dims[0], dims[3], dims[4]],
+            [dims[1], dims[2], dims[4]]
+        ],
+        [
+            [dims[0], dims[3], dims[4]],
+            [dims[1], dims[2], dims[4]],
+            [dims[1], dims[3], dims[4]]
+        ],
+        #Z top face
+        [
+            [dims[0], dims[2], dims[5]],
+            [dims[0], dims[3], dims[5]],
+            [dims[1], dims[2], dims[5]]
+        ],
+        [
+            [dims[0], dims[3], dims[5]],
+            [dims[1], dims[2], dims[5]],
+            [dims[1], dims[3], dims[5]]
+        ],
+        #Y top face
+        [
+            [dims[0], dims[3], dims[4]],
+            [dims[0], dims[3], dims[5]],
+            [dims[1], dims[3], dims[4]]
+        ],
+        [
+            [dims[0], dims[3], dims[5]],
+            [dims[1], dims[3], dims[5]],
+            [dims[1], dims[3], dims[4]]
+        ],
+        #Y bottom face
+        [
+            [dims[0], dims[4], dims[4]],
+            [dims[0], dims[4], dims[5]],
+            [dims[1], dims[4], dims[4]]
+        ],
+        [
+            [dims[0], dims[4], dims[5]],
+            [dims[1], dims[4], dims[5]],
+            [dims[1], dims[4], dims[4]]
+        ],
+        #X right face
+        [
+            [dims[1], dims[2], dims[4]],
+            [dims[1], dims[3], dims[4]],
+            [dims[1], dims[3], dims[5]]
+        ],
+        [
+            [dims[1], dims[2], dims[4]],
+            [dims[1], dims[2], dims[5]],
+            [dims[1], dims[3], dims[5]]
+        ],
+    ])
+
+    return box
