1ded4bee1af86fb100f9ee82a54e044c371c0c01
Thorhian <thorhian@protonmail.com>
2022-08-11 17:07:06 -0600

    Add atomic counters to cShader, increment logic.

--- shaders/count_colors.glsl
+++ shaders/count_colors.glsl
@@ -1,19 +1,23 @@
-#version 460 core
+#version 430 core
 
 layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 layout(rgba32f, binding = 0) uniform image2D imageSlice;
+layout(binding = 1) uniform atomic_uint stock;
+layout(binding = 1) uniform atomic_uint obstacle;
+layout(binding = 1) uniform atomic_uint model;
 uniform vec2 imageSize;
 uniform vec4 circleCenters;
 uniform float circleRadius;
+uniform mat2x4 quadPouints; //Must be sorted in anti-clockwise order.
 
-bool isInsideCircle(float radius, ivec2 centerCoords, ivec2 pixelCoords) {
+bool isInsideCircle(float radius, vec2 centerCoords, ivec2 pixelCoords) {
     float leftSide = pow(pixelCoords[0] - centerCoords[0], 2) +
         pow(pixelCoords[1] - centerCoords[1], 2);
 
     return (leftSide < pow(radius, 2));
 }
 
-bool isInsideQuad(mat2x4 points, vec2 point) {
+bool isInsideQuad(mat2x4 points, ivec2 point) {
     int i = 0;
     while(i < 4) {
         vec2 v1 = points[i];
@@ -37,8 +41,22 @@ void main() {
         return;
     }
 
-    if()
 
     vec4 texColor = texelFetch(imageSlice, texelPosition, 0);
+
+    if((texColor.a >= 0.98) &&
+      isInsideCircle(circleRadius, vec2(circleCenters[0], circleCenters[1]), texelPosition) &&
+      isInsideCircle(circleRadius, vec2(circleCenters[2], circleCenters[3]), texelPosition) &&
+      isInsideQuad(quadPoints, texelPosition)) {
+        if (texColor.b >= 0.98) {
+          atomicCounterIncrement(model);
+          return;
+      } else if (texColor.r >= 0.98) {
+          atomicCounterIncrement(obstacle);
+          return;
+      } else {
+          atomicCounterIncrement(stock);
+      }
+    }
 
 }
