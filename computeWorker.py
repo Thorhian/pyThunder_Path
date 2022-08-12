5ae7fbe6444c57ae3d8b714e3b97aebbaf46ec34
Thorhian <thorhian@protonmail.com>
2022-08-12 16:52:02 -0600

    Switching from atomic counters to SSBO with uints.
    ModernGL currently doesn't seem to support the buffer
    indices that correspond to atomic Counters. Using generic
    SSBO and atomicAdd to perform counting.
    
    Fixing dtypes and binding to SSBO.
    
    Remove debug print statements

--- computeWorker.py
+++ computeWorker.py
@@ -13,22 +13,30 @@ class ComputeWorker:
             target_image: np.ndarray,
             ):
         self.pixel_res = pixel_res
-        self.target_image = target_image
         self.ctx = moderngl.create_standalone_context()
         self.ctx.enable(moderngl.DEPTH_TEST)
-        self.image_res = target_image.shape
+        self.image_res = (target_image.shape[0], target_image.shape[1])
+
+
 
         count_program = hf.load_shader("./shaders/count_colors.glsl")
-        self.counter_compute = self.ctx.compute_shader(count_program)
+        self.counter_compute: moderngl.ComputeShader = self.ctx.compute_shader(count_program)
         self.counter_compute['imageSize'] = self.image_res[0], self.image_res[1]
-        self.image_buffer = self.ctx.texture(self.target_image, 4)
+        self.image_buffer = self.ctx.texture(self.image_res, 4)
+        self.image_buffer.write(target_image)
         self.image_buffer.bind_to_image(1)
-        self.counter_compute['imageSlise'] = 1
+        self.counter_compute['imageSlice'] = 1
+        dtype = np.dtype('u4')
+        uint_counters = np.array([0, 0, 0], dtype=dtype)
+        self.uint_buffer = self.ctx.buffer(uint_counters)
+        self.uint_buffer.bind_to_storage_buffer(1)
+
 
         image_vertex_code = hf.load_shader("./shaders/image_shader.vert")
         paint_frag_code = hf.load_shader("./shaders/painter.frag")
         self.painter_prog = self.ctx.program(vertex_shader=image_vertex_code,
                 fragment_shader=paint_frag_code)
+
         
         image_vertices = np.array([
             -1, 1,
@@ -40,3 +48,51 @@ class ComputeWorker:
         self.painter_vao = self.ctx.vertex_array(self.painter_prog, [
             (imageVerts_vbo, '2f', 'in_position')
             ])
+
+
+    def check_cut(self, center1, center2, radius):
+        self.counter_compute['circleCenters'] = center1[0], center1[1], center2[0], center2[1]
+        self.counter_compute['circleRadius'] = radius
+        quadUniform = self.counter_compute['quadPoints']
+        quadIUniform = self.counter_compute['quadIndices']
+        
+        quad: np.ndarray = self.find_rectangle_points(center1, center2, radius)
+        sorted_quad_indices = self.sort_rectangle_verts(quad)
+        quadUniform.write(quad) #type: ignore
+        quadIUniform.write(sorted_quad_indices) #type: ignore
+    
+
+
+    def find_rectangle_points(self, center1, center2, radius):
+        translated_cent1 = np.array(center1) - np.array(center2) #type: ignore
+        translated_cent2 = np.array(center2) - np.array(center1) #type: ignore
+
+        norm_rad1 = (translated_cent1 / np.linalg.norm(translated_cent1)) * radius
+        norm_rad2 = (translated_cent2 / np.linalg.norm(translated_cent2)) * radius
+
+        trans_point1 = (norm_rad1[1], -norm_rad1[0]) + np.array(center2) #type: ignore
+        trans_point2 = (-norm_rad1[1], norm_rad1[0]) + np.array(center2) #type: ignore
+        trans_point3 = (norm_rad2[1], -norm_rad2[0]) + np.array(center1) #type: ignore
+        trans_point4 = (-norm_rad2[1], norm_rad2[0]) + np.array(center1) #type: ignore
+
+        return np.array([trans_point1, trans_point2, trans_point3, trans_point4], 
+                dtype=np.dtype('f4'))
+
+    def sort_rectangle_verts(self, vertices):
+        avg_center = np.add.reduce(vertices) / 4
+
+        offset_verts = vertices - avg_center
+        polar_rotations = np.zeros(4)
+
+        i = 0
+        while i < 4:
+            rotation = np.rad2deg(np.arctan2(offset_verts[i][1],
+                                             offset_verts[i][0]))
+
+            if rotation < 0:
+                rotation = 360 + rotation
+
+            polar_rotations[i] = rotation
+            i += 1
+
+        return np.argsort(polar_rotations).astype('i4')
--- shaders/count_colors.glsl
+++ shaders/count_colors.glsl
@@ -2,13 +2,15 @@
 
 layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 layout(rgba32f, binding = 0) uniform image2D imageSlice;
-layout(binding = 1) uniform atomic_uint stock;
-layout(binding = 1) uniform atomic_uint obstacle;
-layout(binding = 1) uniform atomic_uint model;
+layout(std430, binding = 1) buffer counterBuffer
+{
+  uint counters[];
+} cIn;
 uniform vec2 imageSize;
 uniform vec4 circleCenters;
 uniform float circleRadius;
-uniform mat2x4 quadPouints; //Must be sorted in anti-clockwise order.
+uniform mat4x2 quadPoints;
+uniform ivec4   quadIndices;
 
 bool isInsideCircle(float radius, vec2 centerCoords, ivec2 pixelCoords) {
     float leftSide = pow(pixelCoords[0] - centerCoords[0], 2) +
@@ -17,12 +19,14 @@ bool isInsideCircle(float radius, vec2 centerCoords, ivec2 pixelCoords) {
     return (leftSide < pow(radius, 2));
 }
 
-bool isInsideQuad(mat2x4 points, ivec2 point) {
+bool isInsideQuad(ivec2 point) {
     int i = 0;
     while(i < 4) {
-        vec2 v1 = points[i];
-        vec2 v2 = points[(i + 1) % 4];
-        d = (v2[0] - v1[0]) * (point[1] - v1[1]) - (point[0] - v1[0]) * (v2[1] - v1[1]);
+        int trueIndice = quadIndices[i];
+        int nextTrue = quadIndices[(i + 1) % 4];
+        vec2 v1 = quadPoints[trueIndice];
+        vec2 v2 = quadPoints[nextTrue];
+        float d = (v2[0] - v1[0]) * (point[1] - v1[1]) - (point[0] - v1[0]) * (v2[1] - v1[1]);
 
         if(d < 0) {
             return false;
@@ -41,21 +45,20 @@ void main() {
         return;
     }
 
-
-    vec4 texColor = texelFetch(imageSlice, texelPosition, 0);
+    vec4 texColor = imageLoad(imageSlice, texelPosition);
 
     if((texColor.a >= 0.98) &&
       isInsideCircle(circleRadius, vec2(circleCenters[0], circleCenters[1]), texelPosition) &&
       isInsideCircle(circleRadius, vec2(circleCenters[2], circleCenters[3]), texelPosition) &&
-      isInsideQuad(quadPoints, texelPosition)) {
+      isInsideQuad(texelPosition)) {
         if (texColor.b >= 0.98) {
-          atomicCounterIncrement(model);
+          atomicAdd(cIn.counters[0], 1); //Model Pixel
           return;
       } else if (texColor.r >= 0.98) {
-          atomicCounterIncrement(obstacle);
+          atomicAdd(cIn.counters[1], 1); //Obstacle Pixel
           return;
       } else {
-          atomicCounterIncrement(stock);
+          atomicAdd(cIn.counters[2], 1); //Stock Pixel
       }
     }
 
