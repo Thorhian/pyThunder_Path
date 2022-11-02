513e76ca3b0570cfac7d297fcf73a9112eb0abd4
Thorhian <thorhian@protonmail.com>
2022-11-02 14:07:27 -0600

    Initial Image Check, TODO: Mask option.

--- shaders/count_image_total.glsl
+++ shaders/count_image_total.glsl
@@ -9,15 +9,44 @@ layout(std430, binding = 1) buffer counterBuffer
 } cIn;
 
 uniform vec4 color1 = vec4(0.0, 0.0, 0.0, 1.0);
+uniform vec4 color2 = vec4(0.0, 0.0, 1.0, 1.0);
+uniform vec4 color3 = vec4(0.0, 1.0, 0.0, 1.0);
 
-void main() {
-  ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-  ivec2 imageDims = imageSize(imageSlice);
+bool compareVecs(vec4 vecA, vec4 vecB, float thresh) {
+  vec4 comparator = abs(vecA - vecB);
+  bool withinThresh = true;
 
-  if(texelPosition.x > (imageDims.x - 1) || texelPosition.y > (imageDims.y - 1)) {
-    return;
+  int i = 0;
+  for(i = 0; i < 4; i++) {
+    if (comparator[i] > thresh) {
+      withinThresh = false;
+    }
   }
 
-  vec4 imgColor  = imageLoad(imageSlice);
+  return withinThresh;
+}
 
+void main() {
+    ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 imageDims = imageSize(imageSlice);
+
+    if(texelPosition.x > (imageDims.x - 1) || texelPosition.y > (imageDims.y - 1)) {
+        return;
+    }
+
+    vec4 texColor = texelFetch(imageSlice, pos, 0);
+
+    if (compareVecs(texColor, color1, 0.2)) {
+        atomicAdd(cIn.counters[0], 1);
+    }
+
+    if (compareVecs(texColor, color2, 0.2)) {
+        atomicAdd(cIn.counters[1], 1);
+    }
+
+    if (compareVecs(texColor, color3, 0.2)) {
+        atomicAdd(cIn.counters[2], 1);
+    }
+
+    atomicAdd(cIn.counters[3], 1); //Total Pixels
 }
