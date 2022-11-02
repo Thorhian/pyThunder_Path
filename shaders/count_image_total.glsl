b09cb25711d2b2faaae655c956510c1b1635bcd4
Thorhian <thorhian@protonmail.com>
2022-11-01 19:32:05 -0600

    Making global counter shader more flexible.

--- shaders/count_image_total.glsl
+++ shaders/count_image_total.glsl
@@ -1,11 +1,14 @@
-#version 430 core
+#version 450 core
 
 layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
-layout(rgba32f, binding = 0) uniform image2D imageSlice;
+layout(rgba32f, binding = 0) uniform sampler2D imageSlice;
+layout(rgba32f, binding = 2) uniform sampler2D mask;
 layout(std430, binding = 1) buffer counterBuffer
 {
   uint counters[];
 } cIn;
+
+uniform vec4 color1 = vec4(0.0, 0.0, 0.0, 1.0);
 
 void main() {
   ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
