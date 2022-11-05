5c59af8e139daf8b5a36a61541b2573495c3954d
Thorhian <thorhian@protonmail.com>
2022-11-05 15:24:39 -0600

    Changed some settings

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import Helper_Functions as hf
 
 isDebugModeOn = False
 # Units should be in Metric.
-target_res_per_pixel = 0.5 #Width/Height of each pixel
+target_res_per_pixel = 0.1 #Width/Height of each pixel
 
 for arg in sys.argv:
     if arg == '--help' or arg == '-h':
@@ -41,5 +41,5 @@ newJob = Job(model_mesh, stock_mesh, [],
              debug=isDebugModeOn)
 print(newJob.bounds)
 newJob.render_layers(depth_of_cut)
-newJob.generate_paths()
 newJob.save_images()
+newJob.generate_paths()
