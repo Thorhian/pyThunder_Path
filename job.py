Fixed numerical errors and location cutting bugs.
Also, added script for printing cPython profile
data.

job.py
@@ -301,13 +301,13 @@ class Job:
         if direction >= 360.0 or direction < 0.0:
             raise Exception(f"direction should be between 0 (inclusive) to 360 (exclusive), not {direction}")
 
-        scan_direction = -1
-        if clockwiseScan == False:
-            scan_direction = 1
+        scan_direction = 1
+        if clockwiseScan == True:
+            scan_direction = -1
             
-        movement_vector = np.array([0, distance])
-        theta = np.radians(direction)
-        theta_inc = np.radians(direction + deg_inc * scan_direction)
+        movement_vector = np.array([1, 0])
+        theta = np.radians((direction) % 360.0)
+        theta_inc = np.radians((deg_inc * scan_direction) % 360.0)
         c, s = np.cos(theta), np.sin(theta)
         c_inc, s_inc = np.cos(theta_inc), np.sin(theta_inc)
 
@@ -319,12 +319,13 @@ class Job:
             increment = np.array(np.dot(scan_rot_v, test_vectors[-1]))
             test_vectors = np.append(test_vectors, [increment], axis=0)
             
+        test_vectors = test_vectors * distance
         test_vectors = test_vectors + coords
         cut_stats = []
         for i in range(iterations):
             cut_stats.append(cw.check_cut(np.flip(coords), np.flip(test_vectors[i]), tool_rad))
 
-        tested_directions = np.arange(direction, direction + (deg_inc * iterations), deg_inc)
+        tested_directions = np.arange(direction, direction + (deg_inc * iterations), (deg_inc * scan_direction))
         tested_directions = np.mod(tested_directions, 360.0)
         return [test_vectors, np.array(cut_stats), tested_directions]
 
@@ -343,16 +344,16 @@ class Job:
         print(f"Image Center: {img_center}")
         worker.make_cut(np.flip(bore_coord), np.flip(bore_coord) + 0.1, (self.tool_diam * 1.5) / self.target_res)
         self.ctx.finish()
-        distance = 0.2
+        distance = 1
         distance_adjusted = distance / self.target_res
         print(f"Distance of cut checking: {distance}mm")
 
         current_direction = 0.0
         materialRemovalRatio = 0.3
-        currentLoc = bore_coord + np.array([tool_radius * 1.5, 0])
+        currentLoc = bore_coord + np.array([7.0 / self.target_res, 0])
         locations = np.array([currentLoc * self.target_res])
         emptyCounter = 0
-        for i in range(100000):
+        for i in range(20000):
             if emptyCounter >= 1000:
                 print("Too much empty cutting, proceed to next area.")
                 break
@@ -365,16 +366,17 @@ class Job:
 
             if i % 1000 == 0:
                 print(f"Iteration: {i}")
-            if i % 10000 == 0:
+                #if i % 10000 == 0:
                 image = Image.fromarray(worker.retrieve_image())
                 image.save(f"./renders/testCut{i}.png")
 
             candidates = self.checkCuts(worker, currentLoc,
                                         direction=current_direction,
                                         tool_rad=tool_radius,
-                                        deg_inc=2,
-                                        iterations=90,
-                                        distance=distance_adjusted)
+                                        deg_inc=0.5,
+                                        iterations=180,
+                                        distance=distance_adjusted,
+                                        clockwiseScan=False)
 
             #print(f"Candidate Cuts:\n{'Model Obstacle  Stock  Total' : >32}\n {candidates[1]}")
             madeCut = False
@@ -383,9 +385,10 @@ class Job:
                 if candidate[0] < 1 and candidate[1] < 1:
                     ratio = candidate[2] / candidate[3]
                     if ratio < materialRemovalRatio:
-                        currentLoc = candidates[0][i]
-                        worker.make_cut(np.flip(bore_coord), np.flip(currentLoc), tool_radius)
+                        new_loc = candidates[0][i]
+                        worker.make_cut(np.flip(currentLoc), np.flip(new_loc), tool_radius)
                         current_direction = candidates[2][i]
+                        currentLoc = new_loc
                         madeCut = True
                         locations = np.append(locations, [currentLoc * self.target_res], axis=0)
                         if ratio < 0.01:
@@ -400,7 +403,6 @@ class Job:
         image = Image.fromarray(worker.retrieve_image())
         image.save(f"./renders/testCut.png")
 
-        print(locations)
         hf.gen_test_gcode(locations)
 
         return 0